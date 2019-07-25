0bea35eb64695417d286a6600551b20e03314dbe
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2019-07-25 02:56:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+// var1 = 2;
+
+// var2 = 31;
+
+let multiply = oldID * currentAge;
+
+let random = Math.floor(Math.random())
+
+let mod = 16 % 4
+
+let max = Math.max(1, 2, 3, 4, 20)
